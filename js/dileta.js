e64adae6f85cba7c7c264bddc5350e48d5aba30a
lektorius <dileta.darbinis@gmail.com>
2017-03-23 21:12:33 +0200

    dileta

--- js/dileta.js
+++ js/dileta.js
@@ -2,7 +2,12 @@
  * Created by lektorius on 23/03/2017.
  */
 function manoFunkcija(kintamasis) {
-    document.getElementById('elementas1'). innerHTML = 'Naujas tekstas ir' +kintamasis;
-
+    document.getElementById('elementas1').innerHTML = 'Naujas tekstas ir' +kintamasis;
+}
+function manoFunkcija2(kintamasis) {
+    console.log(kintamasis);
+    document.getElementById('muse').src = kintamasis;
 }
 
+
+// var image = document.getElementsByName('muse').scr;
