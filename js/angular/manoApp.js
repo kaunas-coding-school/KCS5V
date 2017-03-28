445ecc6b040eeb74efc55f7ae8f221c3b6e21c65
lektorius <dileta.darbinis@gmail.com>
2017-03-28 19:30:22 +0300

    dileta

--- js/angular/manoApp.js
+++ js/angular/manoApp.js
@@ -4,4 +4,5 @@ app.controller('TautvydasController', tautvydasController());
 app.controller('KarolisController', karolisController());
 app.controller('TomasController', tautvydasController());
 app.controller('DziugasController', dziugasController());
+app.controller('DiletaController', DiletaController());
 app.controller('IgnasController', ignasController());
