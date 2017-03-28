700464c2a85fa765a79e2d575ab76ef80e12ad74
Dariuskr <darius.krei@gmail.com>
2017-03-28 19:32:17 +0300

    kontroleriai

--- js/angular/manoApp.js
+++ js/angular/manoApp.js
@@ -5,3 +5,4 @@ app.controller('KarolisController', karolisController());
 app.controller('TomasController', tautvydasController());
 app.controller('DziugasController', dziugasController());
 app.controller('IgnasController', ignasController());
+app.controller('DariusController', dariusController());
