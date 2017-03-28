7a375cc86a453484f06d64e7951b59c3d00612e2
Dziugas <dziugas@graphity.org>
2017-03-28 20:17:43 +0300

    dziugas antradienis

--- js/angular/manoApp.js
+++ js/angular/manoApp.js
@@ -3,7 +3,7 @@ var app = angular.module('manoApp', []);
 app.controller('TautvydasController', TautvydasController());
 app.controller('KarolisController', karolisController());
 app.controller('TomasController', tautvydasController());
-app.controller('DziugasController', dziugasController());
+app.controller('DziugasController', DziugasController());
 app.controller('IgnasController', ignasController());
 app.controller('DariusController', dariusController());
 app.controller('DiletaController', DiletaController());
