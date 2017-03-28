a2f3e68ebde67e14b90eb650d10e51a7532322a6
Vamzdis <janciukas777@gmail.com>
2017-03-28 19:23:43 +0300

    Update

--- js/angular/manoApp.js
+++ js/angular/manoApp.js
@@ -2,3 +2,4 @@ var app = angular.module('manoApp', []);
 
 app.controller('TautvydasController', tautvydasController());
 app.controller('DziugasController', dziugasController());
+app.controller('IgnasController', ignasController());
