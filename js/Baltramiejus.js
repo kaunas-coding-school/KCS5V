957038303602df92380c0351a7d3bfa83a1c11ef
baltrus <bobexass@gmail.com>
2017-03-29 20:03:54 +0300

    Motohospital_rimciau_2017-03-29

--- js/Baltramiejus.js
+++ js/Baltramiejus.js
@@ -4,3 +4,48 @@
 function manoFunkcija(kintamasis) {
     document.getElementById('elementas1').innerHTML = '' + kintamasis;
 }
+
+function BaltramiejusController(){
+    var vm = this;
+    vm.title = 'Antraštė';
+    vm.vardas = 'Baltramiejus';
+    vm.pavarde = 'Jakštys';
+
+    vm.masyvas = [
+        {
+            "_id": "58dbe08242625eeb7e04a549",
+            "index": 0,
+            "age": 20,
+            "name": "Doris Mcconnell",
+            "gender": "female"
+        },
+        {
+            "_id": "58dbe0824bf80b47882646d6",
+            "index": 1,
+            "age": 36,
+            "name": "Claudette Carey",
+            "gender": "female"
+        },
+        {
+            "_id": "58dbe0824a373e4bb43a7058",
+            "index": 2,
+            "age": 39,
+            "name": "Theresa Chase",
+            "gender": "female"
+        },
+        {
+            "_id": "58dbe082b833a6d346e6de2f",
+            "index": 3,
+            "age": 29,
+            "name": "Janet Stafford",
+            "gender": "female"
+        },
+        {
+            "_id": "58dbe082b5c388a0ede96b34",
+            "index": 4,
+            "age": 26,
+            "name": "Hale Jacobs",
+            "gender": "male"
+        }
+    ];
+}
