ab7ba99569da0261a4647e147cbf130cd09aecab
Tautvydas <tautvydas@dulskis.eu>
2017-03-30 21:02:37 +0300

    Atnaujinimai.

--- js/angular/controllers/TautvydasController.js
+++ js/angular/controllers/TautvydasController.js
@@ -1,4 +1,62 @@
 function TautvydasController() {
     var vm = this;
     vm.title = 'Antraštė';
+    vm.vardas = 'Tautvydas2';
+    vm.pavarde = 'Dulskis';
+
+
+    vm.masyvas = [
+        {
+            "index": 0,
+            "balance": "$1,447.39",
+            "picture": "http://placehold.it/32x32",
+            "age": 33,
+            "name": "Tiffany Dean",
+            "state": false,
+            "email": "tiffanydean@geekular.com",
+            "phone": "+1 (930) 567-2157"
+        },
+        {
+            "index": 1,
+            "balance": "$3,852.91",
+            "picture": "http://placehold.it/32x32",
+            "age": 29,
+            "name": "Bernadine Kerr",
+            "state": false,
+            "email": "bernadinekerr@geekular.com",
+            "phone": "+1 (825) 454-3522"
+        },
+        {
+            "index": 2,
+            "balance": "$2,662.79",
+            "picture": "http://placehold.it/32x32",
+            "age": 31,
+            "name": "Mosley French",
+            "state": true,
+            "email": "mosleyfrench@geekular.com",
+            "phone": "+1 (880) 599-2072"
+        },
+        {
+            "index": 3,
+            "balance": "$1,054.69",
+            "picture": "http://placehold.it/32x32",
+            "age": 23,
+            "name": "Bowman Hunter",
+            "state": false,
+            "email": "bowmanhunter@geekular.com",
+            "phone": "+1 (823) 555-3248"
+        },
+        {
+            "index": 4,
+            "balance": "$2,084.77",
+            "picture": "http://placehold.it/32x32",
+            "age": 36,
+            "name": "Liliana Tyson",
+            "state": true,
+            "email": "lilianatyson@geekular.com",
+            "phone": "+1 (891) 506-2160"
+        }
+    ]
+
+
 }
